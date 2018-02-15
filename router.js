351531137173b2851d816cc06c97d5476e2d15f8
Kevin <kevin.babcock@go.tarleton.edu>
2018-02-15 13:44:08 -0600

    Update to style and navigation

---
+++ router.js
@@ -0,0 +1,28 @@
+
+module.exports = {
+
+    route: function(app){
+        rt(app);
+    }
+
+}
+
+rt = function (app) {
+    //Router Functions
+    app.get('/', function (req, res) {
+        res.sendFile(__dirname + '/HTML/index.html');
+    })
+
+    app.get('/home', function (req, res) {
+        res.sendFile(__dirname + '/HTML/home.html');
+    })
+
+    app.get('/styles.css', function (req, res) {
+        res.sendFile(__dirname + '/HTML/styles.css');
+    })
+
+    app.get('/assets/sample.png', function (req, res) {
+        res.sendFile(__dirname + '/assets/sample.png');
+    })
+
+}
